8ca57877aea25f75692d277915762ea8193d0bf5
Carolina Soto <licsotocarolina@gmail.com>
2023-02-24 17:51:05 -0300

    subida de trabajo final arg prog

---
+++ funciones.js
@@ -0,0 +1,68 @@
+let mostrarTextouno = document.getElementById('titulo1');
+let textouno = document.getElementById('infouno');
+
+mostrarTextouno.onclick = mostraruno
+
+
+function mostraruno() {
+  if (textouno.style.display== "inline-block") {
+    textouno.style.display = "none";
+  } else {
+    textouno.style.display = "inline-block";
+  }
+};
+
+let mostrarTextodos = document.getElementById('titulo2');
+let textodos = document.getElementById('infodos');
+
+mostrarTextodos.onclick = mostrardos
+
+
+function mostrardos() {
+  if (textodos.style.display== "inline-block") {
+    textodos.style.display = "none";
+  } else {
+    textodos.style.display = "inline-block";
+  }
+};
+
+
+
+let mostrarTextotres = document.getElementById('titulo3');
+let textotres = document.getElementById('infotres');
+
+mostrarTextotres.onclick = mostrartres
+
+
+function mostrartres() {
+  if (textotres.style.display== "inline-block") {
+    textotres.style.display = "none";
+  } else {
+    textotres.style.display = "inline-block";
+  }
+};
+
+
+
+let mostrarTextocuatro = document.getElementById('titulo4');
+let textocuatro = document.getElementById('infocuatro');
+
+mostrarTextocuatro.onclick = mostrarcuatro
+
+
+function mostrarcuatro() {
+  if (textocuatro.style.display== "inline-block") {
+    textocuatro.style.display = "none";
+  } else {
+    textocuatro.style.display = "inline-block";
+  }
+};
+
+
+
+
+
+
+
+
+
